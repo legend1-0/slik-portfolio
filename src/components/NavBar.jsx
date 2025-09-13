21c2fd8e302dc5903280943d42daa34395cc1f26
legend1-0 <jacobboluwatife9@gmail.com>
2025-09-13 05:31:43 -0700

    change in the import statement

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Menu } from 'lucide-react';
 import { X } from 'lucide-react';
-import SideNav from './sideNav';
+import SideNav from './SideNav';
 const NavBar = () => {
   const [navOpen, setNavOpen] = useState(false)
   const [hamClick, setHamClick] = useState(true)
